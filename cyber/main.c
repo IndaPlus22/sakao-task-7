47facfd789d82d2d6816087676e6a0a50ed9862b
sakao <sakao@kth.se>
2022-11-03 17:44:38 +0100

    i guess hashmaps or something is needed

--- cyber/main.c
+++ cyber/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define NAMESIZE 20
 
@@ -7,11 +8,16 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    char strs[n * 2][NAMESIZE];
+    char strarr[n * 2][NAMESIZE];
+    // char *strarr[n * 2];
+    // char tmpstr[NAMESIZE];
 
     for (int i = 0; i < n * 2; i++)
     {
-        scanf("%s", strs[i]);
+        scanf("%s", strarr[i]);
+        // scanf("%s", tmpstr);
+        // strarr[i] = (char *)malloc(NAMESIZE * sizeof(char));
+        // strcpy(strarr[i], tmpstr);
     }
 
     int j = 0;
@@ -28,7 +34,7 @@ int main()
             for (int q = i + 1; q < n; q++)
             {
                 // printf("p%d: %s %s, p%d: %s %s,   temp[%d]: %d, temp[%d]: %d\n", i, strs[i], strs[i+n], q, strs[q], strs[q+n], i, temp[i], q, temp[q]);
-                if (temp[q] != 1 && strcmp(strs[i], strs[q]) == 0 && strcmp(strs[i + n], strs[q + n]) == 0)
+                if (temp[q] != 1 && strcmp(strarr[i], strarr[q]) == 0 && strcmp(strarr[i + n], strarr[q + n]) == 0)
                 {
                     j++;
                     temp[q] = 1;
@@ -37,6 +43,13 @@ int main()
             }
         }
     }
+
+    // for (int i = 0; i < n*2; i++)
+    // {
+    //     free(strarr[i]);
+    // }
+    // free(strarr);
+    
 
     printf("%d", n - j);
 
